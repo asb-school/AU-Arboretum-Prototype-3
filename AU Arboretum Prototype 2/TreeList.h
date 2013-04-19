b907d878d23b7a3291b9c8e929f59dedba46e670
logwd <logan.david@gmail.com>
2013-04-19 15:25:20 -0300

    Update TreeList.h

--- AU Arboretum Prototype 2/TreeList.h
+++ AU Arboretum Prototype 2/TreeList.h
@@ -3,7 +3,7 @@
 //  MyTreeListIpad
 //
 //  Created by Martin Gaynor on 2/25/13.
-//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2013 Andrews University. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
